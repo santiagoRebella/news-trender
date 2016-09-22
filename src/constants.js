2d29232cf68ec2c6480937d9dd0b1eba5e69c1d4
Santiago Rebella <santiago_rebella@hotmail.com>
2016-09-21 23:46:40 -0300

    takes out old analytics and adsense scripts

--- src/constants.js
+++ src/constants.js
@@ -2,47 +2,15 @@
 
 module.exports = {
   topBanner: `
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- header news trender -->
-    <ins class="adsbygoogle"
-        style="display:inline-block;width:728px;height:90px"
-        data-ad-client="ca-pub-2826838673940502"
-        data-ad-slot="3390089963"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+
   `,
   sideBannerBig: `
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- news-trender side big -->
-    <ins class="adsbygoogle"
-        style="display:inline-block;width:300px;height:600px"
-        data-ad-client="ca-pub-2826838673940502"
-        data-ad-slot="4866823164"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+
   `,
   footerBannerBig: `
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- news-trender footer ad -->
-    <ins class="adsbygoogle"
-        style="display:inline-block;width:970px;height:250px"
-        data-ad-client="ca-pub-2826838673940502"
-        data-ad-slot="6343556366"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+
   `,
   analytics: `
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-84478612-1', 'auto');
-      ga('send', 'pageview');
-    </script>
   `
 };
